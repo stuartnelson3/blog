69564ee383665a6fe3aa64c6e4152b19e5032d94
Stuart Nelson <stuartnelson3@gmail.com>
2013-12-10 19:41:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blog written using gorilla muxer and as much of the stdlib as possible
---
+++ mux.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+    "encoding/json"
+    "github.com/gorilla/mux"
+    "net/http"
+    "fmt"
+    "text/template"
+    "path/filepath"
+    "io/ioutil"
+)
+
+type Post struct {
+    Title       string `json:"title"`
+    Body        string `json:"body"`
+    CreatedAt   string `json:"createdAt"`
+    Slug        string `json:"slug"`
+}
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", rootHandler)
+    r.HandleFunc("/{post}", postHandler)
+    http.Handle("/", r)
+    http.ListenAndServe(":8080", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    absPath, _ := filepath.Abs("on-formatting.json")
+    data, _ := ioutil.ReadFile(absPath)
+    out := &Post{}
+    json.Unmarshal(data, out)
+    t, _ := template.New("foo").Parse(`{{define "T"}}{{.}}{{end}}`)
+    _ = t.ExecuteTemplate(w, "T", out.Body)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    post := vars["post"]
+    fmt.Fprintf(w, "Hello, %q", post)
+}
