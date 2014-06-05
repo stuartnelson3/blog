56fcbd1f35b1caad0c4672ed16e19f018a7826ef
Stuart Nelson <stuartnelson3@gmail.com>
2014-06-04 22:05:34 -0400

    go fmt

--- martiniblog.go
+++ martiniblog.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-    "github.com/codegangsta/martini"
-    "github.com/codegangsta/martini-contrib/render"
-    "path/filepath"
-    "io/ioutil"
-    "encoding/json"
-    "sort"
-    "html/template"
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
 )
 
 type Post struct {
-    Title     string        `json:"title"`
-    Body      template.HTML `json:"body"`
-    Slug      string        `json:"slug"`
-    CreatedAt string        `json:"createdAt"`
-    Mtime     int64         `json:"mtime"`
+	Title     string        `json:"title"`
+	Body      template.HTML `json:"body"`
+	Slug      string        `json:"slug"`
+	CreatedAt string        `json:"createdAt"`
+	Mtime     int64         `json:"mtime"`
 }
 
 func main() {
-    m := martini.Classic()
-    m.Use(render.Renderer(render.Options{
-        Layout:     "layout",
-        Extensions: []string{".html"}}))
+	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Layout:     "layout",
+		Extensions: []string{".html"}}))
 
-    m.Get("/", func(r render.Render) {
-        posts := Post{}.All()
-        r.HTML(200, "index", posts)
-    })
+	m.Get("/", func(r render.Render) {
+		posts := Post{}.All()
+		r.HTML(200, "index", posts)
+	})
 
-    m.Get("/:post", func(params martini.Params, r render.Render) {
-        post := Post{}.Find(params["post"])
-        r.HTML(200, "show", post)
-    })
+	m.Get("/:post", func(params martini.Params, r render.Render) {
+		post := Post{}.Find(params["post"])
+		r.HTML(200, "show", post)
+	})
 
-    m.Run()
+	m.Run()
 }
 
 type ByMtime []*Post
@@ -42,26 +43,26 @@ type ByMtime []*Post
 func (a ByMtime) Len() int      { return len(a) }
 func (a ByMtime) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByMtime) Less(i, j int) bool {
-    return a[i].Mtime > a[j].Mtime
+	return a[i].Mtime > a[j].Mtime
 }
 
 func (p Post) All() []*Post {
-    var posts []*Post
-    matches, _ := filepath.Glob("posts/*.json")
-    for i := 0; i < len(matches); i++ {
-        var post = &Post{}
-        data, _ := ioutil.ReadFile(matches[i])
-        json.Unmarshal(data, post)
-        posts = append(posts, post)
-    }
-    sort.Sort(ByMtime(posts))
-    return posts
+	var posts []*Post
+	matches, _ := filepath.Glob("posts/*.json")
+	for i := 0; i < len(matches); i++ {
+		var post = &Post{}
+		data, _ := ioutil.ReadFile(matches[i])
+		json.Unmarshal(data, post)
+		posts = append(posts, post)
+	}
+	sort.Sort(ByMtime(posts))
+	return posts
 }
 
 func (p Post) Find(slug string) *Post {
-    var post = &Post{}
-    absPath, _ := filepath.Abs("posts/" + slug + ".json")
-    data, _ := ioutil.ReadFile(absPath)
-    json.Unmarshal(data, post)
-    return post
+	var post = &Post{}
+	absPath, _ := filepath.Abs("posts/" + slug + ".json")
+	data, _ := ioutil.ReadFile(absPath)
+	json.Unmarshal(data, post)
+	return post
 }
