8fabd438edac836a006ffb34c2e3059f75a6f2c0
Stuart Nelson <stuartnelson3@gmail.com>
2014-07-29 10:37:23 -0400

    Use basscss and touch up new post page.

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,10 @@ if (location.pathname === "/new_post") {
   var host = location.href.replace(/^http/, 'ws').replace(/new_post$/, 'markdown_preview')
   var ws = new WebSocket(host);
 
-  $(document).on('keyup', 'textarea', function() {
+  $(document).on('keyup', 'textarea, .input', function() {
+    var title = "# " + $('.input').val();
     var body = $('textarea').val();
-    ws.send(body);
+    ws.send(title + "\n" + body);
   });
 
   ws.onmessage = function(e) {
