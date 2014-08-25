53d9849aa08482fb2a2d356b1924c1b7eeccd339
Stuart Nelson <stuartnelson3@gmail.com>
2014-08-25 10:51:50 -0500

    just pushing stuff around.

--- blog.go
+++ blog.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -14,13 +14,16 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+var (
+	posts = allPosts()
+	t, _  = template.ParseGlob("./templates/*.tmpl")
+)
+
 func main() {
 	var (
-		mux   = pat.New()
-		port  = flag.String("p", "3000", "address to bind the server on")
-		dev   = flag.Bool("dev", false, "enable/disable dev mode")
-		posts = allPosts()
-		t, _  = template.ParseGlob("./templates/*.tmpl")
+		mux  = pat.New()
+		port = flag.String("p", "3000", "address to bind the server on")
+		dev  = flag.Bool("dev", false, "enable/disable dev mode")
 	)
 	flag.Parse()
 
@@ -28,51 +31,9 @@ func main() {
 		mux.Get("/new_post", func(w http.ResponseWriter, r *http.Request) {
 			t.ExecuteTemplate(w, "new.tmpl", nil)
 		})
-		mux.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
-			file, header, err := r.FormFile("file")
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-			defer file.Close()
-
-			imgPath, err := CreateImage(file, header)
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(imgPath))
-		})
-		mux.Post("/new_post", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println(r.FormValue("title"))
-			p, err := CreatePost(r.FormValue("title"), r.FormValue("body"))
-			if err != nil {
-				http.Error(w, "Error making post.", 500)
-				return
-			}
-			posts = allPosts()
-			t.ExecuteTemplate(w, "show.tmpl", p)
-		})
-		mux.Get("/markdown_preview", func(w http.ResponseWriter, r *http.Request) {
-			ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-			if _, ok := err.(websocket.HandshakeError); ok {
-				http.Error(w, "Not a websocket handshake", 400)
-				return
-			} else if err != nil {
-				return
-			}
-
-			for {
-				messageType, message, err := ws.ReadMessage()
-				if err != nil {
-					return
-				}
-				if err := ws.WriteMessage(messageType, blackfriday.MarkdownCommon(message)); err != nil {
-					return
-				}
-			}
-		})
+		mux.Post("/upload", errorHandler(upload))
+		mux.Post("/new_post", errorHandler(create))
+		mux.Get("/markdown_preview", errorHandler(markdownPreview))
 	}
 
 	mux.Get("/public/", func(w http.ResponseWriter, r *http.Request) {
@@ -96,4 +57,57 @@ func main() {
 	handler := handlers.LoggingHandler(os.Stdout, mux)
 	handler = handlers.CompressHandler(handler)
 	log.Fatal(http.ListenAndServe(":"+*port, handler))
+}
+
+func errorHandler(fn func(http.ResponseWriter, *http.Request) (int, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if statusCode, err := fn(w, r); err != nil {
+			http.Error(w, err.Error(), statusCode)
+		}
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) (int, error) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		return 500, err
+	}
+	defer file.Close()
+
+	imgPath, err := CreateImage(file, header)
+	if err != nil {
+		return 500, err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(imgPath))
+	return 200, nil
+}
+
+func markdownPreview(w http.ResponseWriter, r *http.Request) (int, error) {
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		return 500, errors.New("Not a websocket handshake")
+	} else if err != nil {
+		return 500, err
+	}
+
+	for {
+		messageType, message, err := ws.ReadMessage()
+		if err != nil {
+			return 500, err
+		}
+		if err := ws.WriteMessage(messageType, blackfriday.MarkdownCommon(message)); err != nil {
+			return 500, err
+		}
+	}
+}
+
+func create(w http.ResponseWriter, r *http.Request) (int, error) {
+	p, err := CreatePost(r.FormValue("title"), r.FormValue("body"))
+	if err != nil {
+		return 500, errors.New("Error making post.")
+	}
+	posts = allPosts()
+	t.ExecuteTemplate(w, "show.tmpl", p)
+	return 200, nil
 }
